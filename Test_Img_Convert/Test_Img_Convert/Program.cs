a10e451aa54ea76f143979b264375b8673e8daec
SenkoVlad <vlad.senko2011@yandex.ru>
2019-11-24 22:23:35 +0300

    v1.0.0

---
+++ Test_Img_Convert/Test_Img_Convert/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Test_Img_Convert
+{
+    class Program
+    {
+        static int counter = 0;
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            Parallel.For(1, 5, StartConvert);
+            sw.Stop();
+            //StartConvert(1);
+
+            Console.WriteLine(counter);
+            Console.WriteLine((sw.ElapsedMilliseconds / 1000.0).ToString());
+            Console.Read();
+        }
+
+        static public void StartConvert(int number)
+        {
+            Bitmap bitmap;
+            ImageConverter converter;
+            foreach (string path in Directory.GetFiles("Img" + number.ToString()))
+            {
+                converter = new ImageConverter();
+                bitmap = new Bitmap(path);
+                Stream imageStream = new MemoryStream();
+                bitmap.Save(imageStream, ImageFormat.Jpeg);
+                byte[] bytePhoto = (byte[])converter.ConvertTo(Image.FromStream(imageStream), typeof(byte[]));
+
+                Console.WriteLine(GetHash(bytePhoto));
+                counter++;
+            }
+        }
+        static public string GetHash(byte[] input)
+        {
+            var md5 = MD5.Create();
+            var hash = md5.ComputeHash(input);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Test_Img_Convert/Test_Img_Convert/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Test_Img_Convert")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test_Img_Convert")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("61bb984e-fd66-4e84-8ec8-4c9a8dc46990")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
